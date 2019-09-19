d4e68bebb80e3378afcc1ef18c2225e0bd33864e
Laura Oppermann <laura.oppermann98@gmx.de>
2019-09-19 16:11:22 +0200

    read more data and livestreaming

--- src/Chart/Chart.java
+++ src/Chart/Chart.java
@@ -8,15 +8,35 @@ public class Chart {
 	
 	int height;
 	int width;
+	int minX;
+	int minY;
+	int maxX;
+	int maxY;
+	int dataRefreshInterval;
 	String title;
 	ArrayList<DataSet> data = new ArrayList<>();
 	
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
 	public void setWidth(int width) {
 		this.width = width;
+	}
+	
+	public void setMinWidth(int minWidth) {
+		this.minX = minWidth;
+	}
+	public void setMinHeight(int minHeight) {
+		this.minY = minHeight;
+	}
+	public void setMaxWidth(int maxWidth) {
+		this.maxX = maxWidth;
+	}
+	public void setMaxHeight(int maxHeight) {
+		this.maxY = maxHeight;
+	}
+	public void setDataRefreshInterval(int interval) {
+		this.dataRefreshInterval = interval;
 	}
 	
 	public void setTitle(String title) {
--- src/DataReader.java
+++ src/DataReader.java
@@ -39,6 +39,11 @@ public class DataReader {
         String cluster_line = "";
         String csvSplitBy = ",";
         
+        Float max_width = 0.00f;
+      	Float max_height = 0.00f;
+      	Float min_width = Float.POSITIVE_INFINITY;
+      	Float min_height= Float.POSITIVE_INFINITY;
+        String location = "";
         /*
          *  save all datapoints in a long list and save all cluster in a Map with date as key
          */
@@ -50,22 +55,31 @@ public class DataReader {
             br_data = new BufferedReader(new FileReader(datapoints));
             br_cluster = new BufferedReader(new FileReader(cluster));
             
+            // read the datapoints into a list
             int count = 0;
-
             while ((data_line = br_data.readLine()) != null) {        
             	count++;
             	if(count == 1) continue;
-            	if(count == 22) break;
+            	if(count == 50) break;
                 String[] data = data_line.split(csvSplitBy);
+            	Float X = Float.parseFloat(data[2]);
+            	Float Y = Float.parseFloat(data[3]);
+            	// search for the max width to set
+            	if(X>max_width) max_width = X;
+            	if(Y>max_height) max_height = Y;
+            	if(X<min_width) min_width = X;
+            	if(Y<min_height) min_height = Y;
+            	
                 // add the current datapoint to list
                 all_datapoints.add(data);
             }
 
+            // read the cluster into a Map
             count = 0;
             while((cluster_line = br_cluster.readLine()) != null ) {
                 count++;
             	if(count == 1) continue;
-            	if(count == 150) break;
+            	if(count == 500) break;
                 String[] cluster_data = cluster_line.split(csvSplitBy);
                 String cluster_date = cluster_data[0];
             	ArrayList<String[]> current_cl_points = new ArrayList<String[]>();
@@ -73,6 +87,13 @@ public class DataReader {
             	if(cluster_map.get(cluster_date) != null) {
                 	current_cl_points = cluster_map.get(cluster_date);
                 }
+            	Float X = Float.parseFloat(cluster_data[2]);
+            	Float Y = Float.parseFloat(cluster_data[3]);
+            	// search for the max width to set
+            	if(X>max_width) max_width = X;
+            	if(Y>max_height) max_height = Y;
+            	if(X<min_width) min_width = X;
+            	if(Y<min_height) min_height = Y;
             	
                 current_cl_points.add(cluster_data);
                 cluster_map.put(cluster_date, current_cl_points);
@@ -92,71 +113,17 @@ public class DataReader {
                 }
             }
         }
-        
-        /*
-         * read dataSet, means:
-         * get list of datapoints( current and up to 20 in the past)
-         * get list of cluster (matching the timestamp of last datapoint)
-         * 
-         * for the beginning: 
-         * 	- points from 0-20
-         * 	- cluster of 20th datapoint
-         */
-        
-        List<String[]> sub_points = all_datapoints.subList(0, 20);
-        String last_point_date = sub_points.get(19)[0];
-        ArrayList<String[]> current_cluster = cluster_map.get(last_point_date);
-        
-        Float max_width = 0.00f;
-      	Float max_height = 0.00f;
-      
-      	ArrayList<DataSet> dataSet = new ArrayList<>();
-        
-        for(int i = 0; i <sub_points.size(); i++) {
-        	
-        	String[] value = sub_points.get(i);
-        	Float X = Float.parseFloat(value[2]);
-        	Float Y = Float.parseFloat(value[3]);
-        	
-        	if(X>max_width) max_width = X;
-        	if(Y>max_height) max_height = Y;
-        	
-    		DataSet currentPoint = new DataSet();
-    		currentPoint.setX(X);
-    		currentPoint.setY(Y);
-    		currentPoint.setRadius(0.1f);
-    		dataSet.add(currentPoint);
-        }
-        for(int i = 0; i <current_cluster.size(); i++) {
-        	
-        	String[] value = current_cluster.get(i);
-        	Float X = Float.parseFloat(value[2]);
-        	Float Y = Float.parseFloat(value[3]);
-        	
-        	if(X>max_width) max_width = X;
-        	if(Y>max_height) max_height = Y;
-        	
-    		DataSet currentPoint = new DataSet();
-    		currentPoint.setX(X);
-    		currentPoint.setY(Y);
-    		String[] radius = value[1].split("=");
-    		currentPoint.setRadius(Float.parseFloat(radius[1]));
-    		dataSet.add(currentPoint);
-        }
-        
-        /*
-         * set chart params and add data
-         */
-		chart.setTitle(last_point_date);
-		chart.setData(dataSet);
-		chart.setWidth(Math.round(max_width)+10);
-		chart.setHeight(Math.round(max_height)+10);
+        // set all the params for the chart
+		chart.setMaxWidth(Math.round(max_width)+10);
+		chart.setMaxHeight(Math.round(max_height)+10);
+		chart.setMinWidth(Math.round(min_width)-10);
+		chart.setMinHeight(Math.round(min_height)-10);
+		chart.setWidth(300);
+		chart.setHeight(130);
+		chart.setDataRefreshInterval(500);
 		
-		/*
-		 * post request to server and send data
-		 * get back chart Id and open manually to see result
-		 */
-    	String       postUrl       = "http://localhost:8000/chart";// put in your url
+		// make a request to initialize the chart
+		String       postUrl       = "http://localhost:8000/chart";// put in your url
 		Gson         gson          = new Gson();
 		HttpClient   httpClient    = HttpClientBuilder.create().build();
 		HttpPost     post          = new HttpPost(postUrl);
@@ -169,16 +136,100 @@ public class DataReader {
     		post.setHeader("Content-type", "application/json");
     		try {
     			HttpResponse  response = httpClient.execute(post);
-    			System.out.print(response);
+    			location = response.getHeaders("Location")[0].getValue().toString();
+    			System.out.printf("Your Chart is at Location: %s", location);
     		}catch(IOException e) {
     			System.out.printf("could not get response, Error: %s", e);
             }
 		}catch(UnsupportedEncodingException e) {
 			System.out.println(e);
 		}	
+		
+        /*
+         * read dataSet, means:
+         * get list of datapoints( current and up to 20 in the past)
+         * get list of cluster (matching the timestamp of last datapoint)
+         * 
+         * for the beginning: 
+         * 	- points from 0-20
+         * 	- cluster of 20th datapoint
+         */
+        
+        // das muss jetzt automatisch immer wieder passieren
+        // cp: index of current_point
+        // fp: index of first point from sublist
+        
+        for(int cp = 0; cp < all_datapoints.size(); cp++) {
+        	int fp = 0;
+        	if(cp > 19) fp = cp - 19;
+        	// get the sublist of 20 last points
+            List<String[]> sub_points = all_datapoints.subList(fp, cp+1);
+            String last_point_date = sub_points.get(sub_points.size()-1)[0];
+            // get all the cluster for the current date
+            ArrayList<String[]> current_cluster = cluster_map.get(last_point_date);
 
-	}
-	
+            // create a new dataset that will be send to the Server
+            ArrayList<DataSet> dataSet = new ArrayList<>();
+          
+            // iterate through all the points and add them to the dataSet
+            for(int i = 0; i <sub_points.size(); i++) {
+	          	String[] value = sub_points.get(i);
+	          	
+	      		DataSet currentPoint = new DataSet();
+	      		currentPoint.setX(Float.parseFloat(value[2]));
+	      		currentPoint.setY(Float.parseFloat(value[3]));
+	      		currentPoint.setRadius(0);
+	      		dataSet.add(currentPoint);
+            }
+            
+            // iterate through all the cluster and add them to the dataSet
+            for(int i = 0; i <current_cluster.size(); i++) {
+          	
+	          	String[] value = current_cluster.get(i);
+	          	
+	      		DataSet currentPoint = new DataSet();
+	      		currentPoint.setX(Float.parseFloat(value[2]));
+	      		currentPoint.setY(Float.parseFloat(value[3]));
+	      		
+	      		String[] radius = value[1].split("=");
+	      		currentPoint.setRadius(Float.parseFloat(radius[1]));
+	      		dataSet.add(currentPoint);
+            }
+            
+            /*
+             * add chart data
+             */
+    		chart.setTitle(last_point_date);
+    		chart.setData(dataSet);
+
+    		
+    		/*
+    		 * post request to server and update data
+    		 * get back chart Id and open manually to see result
+    		 */
+    		
+        	String       updateUrl       = "http://localhost:8000/chart/" + location;// put in your url
+    		gson          = new Gson();
+    		httpClient    = HttpClientBuilder.create().build();
+    		post          = new HttpPost(updateUrl);
+
+    		try {
+        		postingString = new StringEntity(gson.toJson(chart));//gson.tojson() converts your pojo to json
+        		
+        		post.setEntity(postingString);
+        		post.setHeader("Content-type", "application/json");
+        		try {
+        			HttpResponse  response = httpClient.execute(post);
+        			System.out.print(response);
+        		}catch(IOException e) {
+        			System.out.printf("could not get response, Error: %s", e);
+                }
+    		}catch(UnsupportedEncodingException e) {
+    			System.out.println(e);
+    		}
+    		Thread.sleep(500);
+        }
+	}	
 }
 
 
